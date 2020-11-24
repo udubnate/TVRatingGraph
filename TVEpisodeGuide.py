347ce1bc7614a0f615c8686b04fc25de779000e6
udubnate <natebruneau@gmail.com>
2020-11-23 17:09:09 -0800

    first checkin test

---
+++ TVEpisodeGuide.py
@@ -0,0 +1,9 @@
+# Need to figure out how to connect to a IMDB dataset
+
+# need to figure out how to graph the dataset
+
+# Test scenario. Community and The Office
+# I want to be able to graph each Season, EP0101 show the average review in a bar or line chart. 
+# Be able to click on it to show what the epsiode (or hover) details are
+
+print("Hello World")
